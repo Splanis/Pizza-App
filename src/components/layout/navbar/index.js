6c875e2cd6b00d734c7cc5cf94f99c01ee3b5ac8
Marios G <splanis5@gmail.com>
2020-03-02 21:00:21 +0200

    added second navigation

--- src/components/layout/navbar/index.js
+++ src/components/layout/navbar/index.js
@@ -4,38 +4,55 @@ import { Link } from "react-router-dom";
 import { DropdownItems } from "./DropdownItems";
 import { categories } from "../../../store/categories";
 
-import { Menu, Button, Dropdown, Input, Icon } from "semantic-ui-react";
+import { Menu, Button, Input, Icon } from "semantic-ui-react";
+
+const quantity = {
+    backgroundColor: "red",
+    color: "white",
+    fontSize: 16,
+    fontWeight: 700,
+    position: "absolute",
+    borderRadius: "50%",
+    height: 25,
+    width: 25,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    transform: "translate(100%, -180%)"
+};
 
 export const Navbar = props => {
     return (
-        <Menu inverted>
-            <Menu.Item style={{ marginRight: "auto" }}>
-                <Dropdown item text="Κατηγορίες">
-                    <Dropdown.Menu>
-                        {categories.map(category => (
-                            <DropdownItems key={category.id} category={category} />
-                        ))}
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Menu.Item>
-
-            <Menu.Item style={{ margin: "auto" }}>
-                <Input placeholder="Αναζήτηση..." />
-            </Menu.Item>
-
-            <Menu.Item style={{ marginLeft: "auto" }}>
-                <Link to="/login">
-                    <Button style={{ margin: 5 }} primary>
-                        Σύνδεση
-                    </Button>
-                </Link>
-                <Link to="/register">
-                    <Button style={{ margin: 5 }}>Εγγραφή</Button>
-                </Link>
-                <Link to="/cart">
-                    <Icon style={{ margin: 5 }} size="big" name="shopping cart" />
-                </Link>
-            </Menu.Item>
-        </Menu>
+        <>
+            <Menu style={{ margin: 0, height: 60 }}>
+                <Menu.Item position="left">
+                    <p>This is Logo</p>
+                </Menu.Item>
+                <Menu.Item position="center">
+                    <Input placeholder="Αναζήτηση..." />
+                </Menu.Item>
+                <Menu.Item position="right">
+                    <Link to="/login">
+                        <Button style={{ margin: 5 }} primary>
+                            Σύνδεση
+                        </Button>
+                    </Link>
+                    <Link to="/register">
+                        <Button style={{ margin: 5 }}>Εγγραφή</Button>
+                    </Link>
+                    <Link to="/cart">
+                        <Icon style={{ margin: 5, position: "relative" }} size="big" name="shopping cart" />
+                        <span style={quantity}>0</span>
+                    </Link>
+                </Menu.Item>
+            </Menu>
+            <Menu inverted style={{ marginTop: 0, height: 40, borderRadius: 0 }}>
+                <Menu.Item>
+                    {categories.map(category => (
+                        <DropdownItems key={category.id} category={category} />
+                    ))}
+                </Menu.Item>
+            </Menu>
+        </>
     );
 };
