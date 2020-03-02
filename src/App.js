76aff0826de63e7cbc3e40796b04e5e548b07150
Marios G <splanis5@gmail.com>
2020-03-02 19:48:32 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      Hello Word
-    </div>
-  );
-}
+import { HomePage } from "./pages/homepage";
+import { Navbar } from "./components/navbar";
+import { Register } from "./pages/register";
+import { Login } from "./pages/login";
+import { Cart } from "./pages/cart";
+import { Category } from "./pages/category";
+
+import { Container } from "semantic-ui-react";
+
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Container style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100%" }}>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/cart" component={Cart} />
+                    <Route exact path="/category/:category/:subcategory" component={Category} />
+                </Switch>
+            </Container>
+        </Router>
+    );
+};
 
 export default App;
