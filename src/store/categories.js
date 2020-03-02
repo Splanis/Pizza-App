ca28d212375d965ba8cf53006ae9b908e22d73cb
Marios G <splanis5@gmail.com>
2020-03-02 19:49:09 +0200

    prototype categories created

---
+++ src/store/categories.js
@@ -0,0 +1,32 @@
+export const categories = [
+    {
+        id: 1,
+        name: "Hardware",
+        subcategories: [
+            { id: 1, name: "Επεξεργαστές" },
+            { id: 2, name: "Κάρτες Γραφικών" },
+            { id: 3, name: "Μητρικές Κάρτες" },
+            { id: 4, name: "Τροφοδοτικά" },
+            { id: 5, name: "Κουτιά" },
+            { id: 6, name: "Μνήμες RAM" },
+            { id: 7, name: "Δίσκοι HDD" },
+            { id: 8, name: "Δίσκοι SSD" }
+        ]
+    },
+    {
+        id: 2,
+        name: "Περιφερειακά",
+        subcategories: [
+            { id: 1, name: "Οθόνες" },
+            { id: 2, name: "Πολυμέσα" }
+        ]
+    },
+    {
+        id: 3,
+        name: "Gaming",
+        subcategories: [
+            { id: 1, name: "Ποντίκια" },
+            { id: 2, name: "Πληκτρολόγια" }
+        ]
+    }
+];
