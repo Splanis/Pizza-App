2433316dd5edbb916f552abbc0616941d6150fcd
Marios G <splanis5@gmail.com>
2020-03-04 01:02:30 +0200

    Routes imported from routes.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { HomePage } from "./pages/homepage";
-import { Navbar } from "./components/layout/navbar";
-import { Register } from "./pages/register";
-import { Login } from "./pages/login";
-import { Cart } from "./pages/cart";
-import { Category } from "./pages/category";
+import Routes from "./routes";
+import Navbar from "./components/layout/navbar";
 
 import { Container } from "semantic-ui-react";
 
@@ -15,13 +11,7 @@ const App = () => {
         <Router>
             <Navbar />
             <Container style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100%" }}>
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/cart" component={Cart} />
-                    <Route exact path="/category/:category/:subcategory" component={Category} />
-                </Switch>
+                <Routes />
             </Container>
         </Router>
     );
