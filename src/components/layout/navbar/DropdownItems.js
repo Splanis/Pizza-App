a31d45eb460708e8f58cd23b5562c6f7de9d85e7
Marios G <splanis5@gmail.com>
2020-03-04 01:59:42 +0200

    Link fixed

--- src/components/layout/navbar/DropdownItems.js
+++ src/components/layout/navbar/DropdownItems.js
@@ -6,14 +6,14 @@ import { Dropdown } from "semantic-ui-react";
 export const DropdownItems = ({ category }) => {
     return (
         <>
-            <Dropdown item text={category.name} style={{ width: "200px" }}>
+            <Dropdown text={category.name} style={{ width: "200px" }}>
                 <Dropdown.Menu>
                     {category.subcategories.map(subcategory => (
-                        <Dropdown.Item key={subcategory.id}>
-                            <Link to={`/category/${category.name}/${subcategory.name}`} style={{ color: "black" }}>
+                        <Link to={`/category/${category.name}/${subcategory.name}`}>
+                            <Dropdown.Item key={subcategory.id} style={{ color: "black" }}>
                                 {subcategory.name}
-                            </Link>
-                        </Dropdown.Item>
+                            </Dropdown.Item>
+                        </Link>
                     ))}
                 </Dropdown.Menu>
             </Dropdown>
