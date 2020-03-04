ec2f76ad2f3a60e538d80ae40f4a59de940cdf5b
Marios G <splanis5@gmail.com>
2020-03-04 19:21:58 +0200

    Footer Added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Routes from "./routes";
 import Navbar from "./components/layout/navbar";
+import Footer from "./components/layout/footer";
 
-import { Container } from "semantic-ui-react";
+import { Container } from "./components/shared/Container";
 
 const App = () => {
     return (
         <Router>
             <Navbar />
-            <Container style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100%" }}>
+            <Container>
                 <Routes />
             </Container>
+            <Footer />
         </Router>
     );
 };
---
+++ src/components/layout/footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+    return <div style={{ bottom: 0, postition: "fixed", background: "black", height: 70, width: "100%" }}>Footer</div>;
+};
+
+export default Footer;
