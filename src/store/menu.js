191d01cf298895a3f27d2a8624afe17e3e18a51a
Marios G <splanis5@gmail.com>
2020-03-05 00:17:38 +0200

    menu initialized

---
+++ src/store/menu.js
@@ -0,0 +1,128 @@
+{
+    "menu": {
+        "categories": {
+            "offers": {},
+            "salads": {},
+            "pasta": {},
+            "pizzas": [
+                {
+                    "id": 1001,
+                    "name": "Siciliana",
+                    "phoot": "https://drive.google.com/uc?id=1TAMSqpEuZfjbaZ4DRlbjGQQONl59WOp-",
+                    "price": "6.60",
+                    "ingredients": "Mozzarella, φρέσκια ντομάτα, mix μπαχαρικών, ελαιόλαδο, μαϊντανός, καλαμπόκι, μπέικον"
+                },
+                {
+                    "id": 1002,
+                    "name": "Βεζούβιος",
+                    "photo": "https://drive.google.com/uc?id=1-1wFYfl09XNPR7dkht56oprasyLICSeC",
+                    "price": "6.20",
+                    "ingredients": "Mexican sauce, Mozzarella, ταμπάσκο, πεπερόνι"
+                },
+                {
+                    "id": 1003,
+                    "name": "Μαντόνα",
+                    "photo": "https://drive.google.com/uc?id=1Tf6d5gV-HK8PTbQ5rW7401A98qy7SGTO",
+                    "price": "5.20",
+                    "ingredients": "Mozzarella, φρέσκια ντομάτα, mix μπαχαρικών, ελαιόλαδο, μαϊντανός, ρίγανη"
+                },
+                {
+                    "id": 1004,
+                    "name": "Bianca Chicken Philadelhpia",
+                    "photo": "https://drive.google.com/uc?id=18-nULHU_1Pj32a8QQvdWeR0zC9F7Lbbb",
+                    "price": "6,80",
+                    "ingredients": "Με ροζέτες Philadelphia, Mozzarella, γραβιέρα Νάξου ΠΟΠ, φιλέτο κοτόπουλο και καραμελωμένο κρεμμύδι"
+                },
+                {
+                    "id": 1005,
+                    "name": "Tex BBQ",
+                    "photo": "https://drive.google.com/uc?id=1NVPrpcHdI6vpuLcfEaMg8hrH2AU0Ml4g",
+                    "price": "8.70",
+                    "ingredients": "Σάλτσα BBQ, Gouda, φιλέτο κοτόπουλο, φρέσκα μανιτάρια, φρέσκια ντομάτα, πράσινη πιπεριά"
+                },
+                {
+                    "id": 1006,
+                    "name": "Spicy Fan",
+                    "photo": "https://drive.google.com/uc?id=1ZvGcj50cKcfYRigB75OeXOVAZfRkNlwO",
+                    "price": "8.10",
+                    "ingredients": "Σάλτσα BBQ, Gouda, κρεμμύδι, φιλέτο κοτόπουλο, πατάτες Crosscut, καυτερή πιπεριά, καλαμπόκι"
+                },
+                {
+                    "id": 1007,
+                    "name": "Italian Fan Bolognese",
+                    "photo": "https://drive.google.com/uc?id=156ZkReQMHi-aRn2U__CIevm05lnGyYH7",
+                    "price": "7.80",
+                    "ingredients": "Με λεπτή crispy ζύμη. Σάλτσα bolognese, mozzarella"
+                },
+                {
+                    "id": 1008,
+                    "name": "Καπνιστή",
+                    "photo": "https://drive.google.com/uc?id=1JyhTffih4kmIop8dsiHLfVndvcNOn7Uh",
+                    "price": "8.20",
+                    "ingredients": "Σάλτσα ντομάτας, Gouda, γαλοπούλα, καπνιστό χοιρινό, κρεμμύδι"
+                },
+                {
+                    "id": 1009,
+                    "name": "Καυτερή Beertuosa",
+                    "photo": "https://drive.google.com/uc?id=1XxUO1mGItTCN22TFQTyWDXzycYqJEIT-",
+                    "price": "8.50",
+                    "ingredients": "Λευκή σάλτσα, τυρί gouda, καπνιστό χοιρινό, πεπερόνι, σαλάμι αέρος, λουκάνικο, καυτερή πιπεριά"
+                },
+                {
+                    "id": 1010,
+                    "name": "4 Τυριά",
+                    "photo": "https://drive.google.com/uc?id=1-9-aXxZLd4MXMcA76xJJ4xI62lFxz3aA",
+                    "price": "8.50",
+                    "ingredients": "Λευκή σάλτσα, Gouda, flakes παρμεζάνας, κεφαλοτύρι, Μozzarella"
+                },
+                {
+                    "id": 1011,
+                    "name": "Μεσογειακή",
+                    "photo": "https://drive.google.com/uc?id=1ugnWD53Y16Q1lapb_IeMyGCWtUIyWGm3",
+                    "price": "8.50",
+                    "ingredients": "Σάλτσα ντομάτας, Gouda, κρεμμύδι, φρέσκα μανιτάρια, φρέσκια ντομάτα, ελιές, κεφαλοτύρι, νιφάδες φέτας ΠΟΠ Τρίπολης"
+                },
+                {
+                    "id": 1012,
+                    "name": "Mexicana",
+                    "photo": "https://drive.google.com/uc?id=1yp8Ycgut5VTqHPdsU5ZSO_98eLAX6iO0",
+                    "price": "8.50",
+                    "ingredients": "Mexican sauce, Gouda, μπέικον, φρέσκια ντομάτα, κρεμμύδι, καλαμπόκι, πράσινη πιπεριά, καυτερή πιπεριά"
+                },
+                {
+                    "id": 1013,
+                    "name": "Εξοχής",
+                    "photo": "https://drive.google.com/uc?id=1T80ISOfsBfGEhrvmaoFGugucCmpoWoFx",
+                    "price": "7.80",
+                    "ingredients": "Mexican sauce, Gouda, μπέικον, φρέσκια ντομάτα, κρεμμύδι, καλαμπόκι, πράσινη πιπεριά, καυτερή πιπεριά"
+                },
+                {
+                    "id": 1014,
+                    "name": "Κλασική Special",
+                    "photo": "https://drive.google.com/uc?id=1VTgLTUCj0WJpk_b0KU0FhICxuMaDCTWD",
+                    "price": "8.50",
+                    "ingredients": "Σάλτσα ντομάτας, Gouda, πεπερόνι, λουκάνικο, ζαμπόν, μπέικον, φρέσκα μανιτάρια, πράσινη πιπεριά"
+                },
+                {
+                    "id": 1015,
+                    "name": "Παραδοσιακή",
+                    "photo": "https://drive.google.com/uc?id=1a8E_EiO6w3nORbvmQPyrDvoFR9pWWsng",
+                    "price": "6.90",
+                    "ingredients": "Σάλτσα ντομάτας, Gouda, ζαμπόν, μπέικον, φρέσκα μανιτάρια, πράσινη πιπεριά"
+                },
+                {
+                    "id": 1016,
+                    "name": "Buffalo Chicken",
+                    "photo": "https://drive.google.com/uc?id=1Z_coYE6WGh_R1Oif98MFixVec9rwlNMg",
+                    "price": "8.50",
+                    "ingredients": "Blue cheese sauce, τυρί mozzarella, κοτομπουκιές, μπέικον, sriracha mild sauce."
+                }
+            ],
+            "my_pizza": {},
+            "vegan": {},
+            "appetizers": {},
+            "deserts": {},
+            "drinks": {}
+        }
+    }
+}
