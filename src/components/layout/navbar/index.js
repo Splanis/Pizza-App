42dce05f332106c108bcd226007521e423aeafba
Marios G <splanis5@gmail.com>
2020-03-04 02:01:15 +0200

    Navbar added links

--- src/components/layout/navbar/index.js
+++ src/components/layout/navbar/index.js
@@ -1,50 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useSelector } from "react-redux";
+
 import { DropdownItems } from "./DropdownItems";
 import { categories } from "../../../store/categories";
+import AuthLinks from "./AuthLinks";
+import NotAuthLinks from "./NotAuthLinks";
 
-import { Menu, Button, Input, Icon } from "semantic-ui-react";
+import { Menu, Input } from "semantic-ui-react";
 
-const quantity = {
-    backgroundColor: "red",
-    color: "white",
-    fontSize: 16,
-    fontWeight: 700,
-    position: "absolute",
-    borderRadius: "50%",
-    height: 25,
-    width: 25,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    transform: "translate(100%, -180%)"
-};
+const Navbar = () => {
+    const isUser = useSelector(state => (state.firebase.auth.uid ? true : false));
+    const userFirstName = useSelector(state => state.firebase.profile.firstName);
 
-export const Navbar = props => {
     return (
         <>
             <Menu style={{ margin: 0, height: 60 }}>
-                <Menu.Item position="left">
-                    <p>This is Logo</p>
-                </Menu.Item>
-                <Menu.Item position="center">
+                <Link to="/" style={{ display: "flex", justifyContent: "center" }}>
+                    <Menu.Item position="left">
+                        <p>This is Logo</p>
+                    </Menu.Item>
+                </Link>
+                <Menu.Item position="right">
                     <Input placeholder="Αναζήτηση..." />
                 </Menu.Item>
-                <Menu.Item position="right">
-                    <Link to="/login">
-                        <Button style={{ margin: 5 }} primary>
-                            Σύνδεση
-                        </Button>
-                    </Link>
-                    <Link to="/register">
-                        <Button style={{ margin: 5 }}>Εγγραφή</Button>
-                    </Link>
-                    <Link to="/cart">
-                        <Icon style={{ margin: 5, position: "relative" }} size="big" name="shopping cart" />
-                        <span style={quantity}>0</span>
-                    </Link>
-                </Menu.Item>
+                <Menu.Item position="right">{isUser ? <AuthLinks userFirstName={userFirstName} /> : <NotAuthLinks />}</Menu.Item>
             </Menu>
             <Menu inverted style={{ marginTop: 0, height: 40, borderRadius: 0 }}>
                 <Menu.Item>
@@ -56,3 +37,5 @@ export const Navbar = props => {
         </>
     );
 };
+
+export default Navbar;
