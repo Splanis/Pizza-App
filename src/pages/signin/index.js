cca6c197d5a3742fee5d2f150b368326414cd5d6
Marios G <splanis5@gmail.com>
2020-03-04 19:22:40 +0200

    Autherrors added

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,50 +1,102 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signInAction } from "../../store/actions/authActions";
 
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Form, Segment, Grid, Divider, Input } from "semantic-ui-react";
 
 const SignIn = () => {
+    const [errors, setError] = useState({
+        emailError: "",
+        passwordError: ""
+    });
+    const { emailError, passwordError } = errors;
+
     const [credentials, setCredentials] = useState({
         email: "",
         password: ""
     });
-
     const { email, password } = credentials;
 
     const dispatch = useDispatch();
-    const signIn = credentials => dispatch(signInAction(credentials));
+    const authError = useSelector(state => state.auth.authError);
+
+    useEffect(() => {
+        // Translating AuthErrors in Greek
+        switch (authError) {
+            case "auth/invalid-email":
+                setError({ passwordError: "", emailError: "Λάθος Email" });
+                break;
+            case "auth/wrong-password":
+                setError({ emailError: "", passwordError: "Λάθος κωδικός" });
+                break;
+            default:
+                setError({ ...errors });
+        }
+    }, [authError]);
 
     const handleSubmit = e => {
         e.preventDefault();
-        signIn({
-            email: email,
-            password: password
-        });
+        dispatch(
+            signInAction({
+                email: email,
+                password: password
+            })
+        );
     };
 
     return (
-        <Segment style={{ width: 300 }}>
-            <Form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
-                <Form.Field>
-                    <label>Email</label>
-                    <input type="text" value={email} onChange={e => setCredentials({ ...credentials, email: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Κωδικός</label>
-                    <input type="password" value={password} onChange={e => setCredentials({ ...credentials, password: e.target.value })} />
-                </Form.Field>
-                <div style={{ margin: "auto" }}>
-                    <Button primary type="submit">
-                        Σύνδεση
-                    </Button>
+        <Segment placeholder>
+            <Grid columns={2} relaxed="very" stackable>
+                <Grid.Column>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Field
+                            control={Input}
+                            icon="user"
+                            iconPosition="left"
+                            label={"Email"}
+                            value={email}
+                            onChange={e => setCredentials({ ...credentials, email: e.target.value })}
+                            error={
+                                emailError
+                                    ? {
+                                          content: emailError
+                                      }
+                                    : null
+                            }
+                        />
+                        <Form.Field
+                            control={Input}
+                            icon="lock"
+                            iconPosition="left"
+                            label="Κωδικός"
+                            type="password"
+                            value={password}
+                            onChange={e => setCredentials({ ...credentials, password: e.target.value })}
+                            error={
+                                passwordError
+                                    ? {
+                                          content: passwordError
+                                      }
+                                    : null
+                            }
+                        />
+
+                        <Button primary type="submit">
+                            Σύνδεση
+                        </Button>
+                    </Form>
+                </Grid.Column>
+
+                <Grid.Column verticalAlign="middle">
                     <Link to="/register">
                         <Button type="submit">Εγγραφή</Button>
                     </Link>
-                </div>
-            </Form>
+                </Grid.Column>
+            </Grid>
+
+            <Divider vertical>Ή</Divider>
         </Segment>
     );
 };
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signUpAction } from "../../store/actions/authActions";
 
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Form, Segment, Input } from "semantic-ui-react";
 
 const SignUp = () => {
+    const [errors, setError] = useState({
+        emailError: "",
+        passwordError: ""
+    });
+    const { emailError, passwordError } = errors;
+
     const [credentials, setCredentials] = useState({
         email: "",
         password: "",
@@ -16,58 +22,123 @@ const SignUp = () => {
         phone: "",
         postal_code: ""
     });
-
     const { email, password, firstName, lastName, city, address, postal_code, phone } = credentials;
 
     const dispatch = useDispatch();
-    const signUp = credentials => dispatch(signUpAction(credentials));
+    const authError = useSelector(state => state.auth.authError);
+
+    useEffect(() => {
+        // Translating AuthErrors in Greek
+        switch (authError) {
+            case "auth/invalid-email":
+                setError({ passwordError: "", emailError: "Το Email είναι λάθος" });
+                break;
+            case "auth/email-already-in-use":
+                setError({ passwordError: "", emailError: "Το Email χρησιμοποιείται ήδη" });
+                break;
+            case "auth/weak-password":
+                setError({ emailError: "", passwordError: "Αδύναμος κωδικός" });
+                break;
+            default:
+                setError({ ...errors });
+        }
+    }, [authError]);
 
     const handleSubmit = e => {
         e.preventDefault();
-        signUp(credentials);
+        dispatch(signUpAction(credentials));
     };
 
     return (
-        <Segment style={{ width: 500 }}>
-            <Form onSubmit={handleSubmit}>
-                <Form.Field>
-                    <label>Email</label>
-                    <input type="text" value={email} onChange={e => setCredentials({ ...credentials, email: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Κωδικός</label>
-                    <input type="password" value={password} onChange={e => setCredentials({ ...credentials, password: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Όνομα</label>
-                    <input type="text" value={firstName} onChange={e => setCredentials({ ...credentials, firstName: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Επώνυμο</label>
-                    <input type="text" value={lastName} onChange={e => setCredentials({ ...credentials, lastName: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Πόλη</label>
-                    <input type="text" value={city} onChange={e => setCredentials({ ...credentials, city: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Διεύθυνση</label>
-                    <input type="text" value={address} onChange={e => setCredentials({ ...credentials, address: e.target.value })} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Τ.Κ</label>
-                    <input
+        <Segment style={{ width: 400 }} placeholder>
+            <Form unstackable onSubmit={handleSubmit}>
+                <Form.Group widths={2}>
+                    <Form.Field
+                        control={Input}
+                        type="text"
+                        value={email}
+                        onChange={e => setCredentials({ ...credentials, email: e.target.value })}
+                        label="Email"
+                        error={
+                            emailError
+                                ? {
+                                      content: emailError
+                                  }
+                                : null
+                        }
+                        required
+                    ></Form.Field>
+                    <Form.Field
+                        control={Input}
+                        type="password"
+                        value={password}
+                        onChange={e => setCredentials({ ...credentials, password: e.target.value })}
+                        label="Κωδικός"
+                        error={
+                            passwordError
+                                ? {
+                                      content: passwordError
+                                  }
+                                : null
+                        }
+                        required
+                    ></Form.Field>
+                </Form.Group>
+                <Form.Group widths={2}>
+                    <Form.Field
+                        control={Input}
+                        type="text"
+                        value={firstName}
+                        onChange={e => setCredentials({ ...credentials, firstName: e.target.value })}
+                        label="Όνομα"
+                        required
+                    ></Form.Field>
+                    <Form.Field
+                        control={Input}
+                        type="text"
+                        value={lastName}
+                        onChange={e => setCredentials({ ...credentials, lastName: e.target.value })}
+                        label="Επώνυμο"
+                        required
+                    ></Form.Field>
+                </Form.Group>
+                <Form.Group widths={2}>
+                    <Form.Field
+                        control={Input}
+                        type="text"
+                        value={city}
+                        onChange={e => setCredentials({ ...credentials, city: e.target.value })}
+                        label="Πόλη"
+                        required
+                    ></Form.Field>
+                    <Form.Field
+                        control={Input}
+                        type="text"
+                        value={address}
+                        onChange={e => setCredentials({ ...credentials, address: e.target.value })}
+                        label="Διεύθυνση"
+                        required
+                    ></Form.Field>
+                </Form.Group>
+                <Form.Group widths={2}>
+                    <Form.Field
+                        control={Input}
                         type="text"
                         value={postal_code}
                         onChange={e => setCredentials({ ...credentials, postal_code: e.target.value })}
-                    />
-                </Form.Field>
-
-                <Form.Field>
-                    <label>Τηλέφωνο</label>
-                    <input type="text" value={phone} onChange={e => setCredentials({ ...credentials, phone: e.target.value })} />
-                </Form.Field>
-                <Button primary type="submit">
+                        label="Τ.Κ"
+                        required
+                    ></Form.Field>
+                    <Form.Field
+                        control={Input}
+                        type="text"
+                        value={phone}
+                        onChange={e => setCredentials({ ...credentials, phone: e.target.value })}
+                        label="Τηλέφωνο"
+                        required
+                    ></Form.Field>
+                </Form.Group>
+                <Button primary type="submit" style={{ justifyContent: "center" }}>
                     Εγγραφή
                 </Button>
             </Form>
