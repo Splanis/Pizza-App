fbd50732c873c41f83f7705f759af0cfb3443856
Marios G <splanis5@gmail.com>
2020-03-05 21:17:28 +0200

    dashboard products added

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+import { Grid, Menu, Segment } from "semantic-ui-react";
+
+import Item from "./item";
+import Cart from "./cart";
 
 const Dashboard = props => {
-    const stores = useSelector(state => state.stores);
-    const dispatch = useDispatch();
+    const [activeItem, setActiveItem] = useState("Πίτσες");
+    const menu = useSelector(state => state.menu);
 
-    return <div>Dashboard</div>;
+    return (
+        <Grid>
+            <Grid.Column width={2}>
+                <Menu fluid vertical tabular>
+                    {menu.categories.map(category => (
+                        <Menu.Item
+                            key={category.id}
+                            name={category.name}
+                            active={activeItem === category.name}
+                            onClick={() => setActiveItem(category.name)}
+                        />
+                    ))}
+                </Menu>
+            </Grid.Column>
+            <Grid.Column stretched width={12}>
+                <Segment style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center" }}>
+                    {menu.categories
+                        .find(category => category.name === activeItem)
+                        .items.map(item => (
+                            <Item
+                                key={item.id}
+                                id={item.id}
+                                photo={item.photo}
+                                name={item.name}
+                                ingredients={item.ingredients}
+                                price={item.price}
+                            />
+                        ))}
+                </Segment>
+            </Grid.Column>
+
+            <Grid.Column width={2}>
+                <Cart />
+            </Grid.Column>
+        </Grid>
+    );
 };
 
 export default Dashboard;
