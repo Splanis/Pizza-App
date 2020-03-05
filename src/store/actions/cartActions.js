2406471532b58dd0aa05bcad7cb2efe5e9b5d4e6
Marios G <splanis5@gmail.com>
2020-03-05 21:20:07 +0200

    cartActions initialized

---
+++ src/store/actions/cartActions.js
@@ -0,0 +1,17 @@
+import * as actions from "../actions/actionTypes";
+
+export const addToCartAction = ({ id, name, photo, ingredients, price }) => async (dispatch, getState) => {
+    dispatch({ type: actions.ADD_TO_CART, item: { id, name, photo, ingredients, price } });
+};
+
+export const removeFromCartAction = ({ id, name, photo, ingredients, price }) => async (dispatch, getState) => {
+    dispatch({ type: actions.REMOVE_FROM_CARD, item: { id, name, photo, ingredients, price } });
+};
+
+export const incrementQuantityAction = id => async (dispatch, getState) => {
+    dispatch({ type: actions.INCREMENT_QUANTITY, item: id });
+};
+
+export const decrementQuantityAction = id => async (dispatch, getState) => {
+    dispatch({ type: actions.DECREMENT_QUANTITY, item: id });
+};
