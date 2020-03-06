b734330bbd9578494ec98bbf6a44b57a51c2f5de
Marios G <splanis5@gmail.com>
2020-03-06 14:15:58 +0200

    added state to localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -7,9 +7,37 @@ import firebase from "./../firebase/firebase";
 
 import rootReducer from "./reducers/rootReducer";
 
+// Load state to local storage
+const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem("state");
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const persistedState = loadFromLocalStorage();
+
+// Creating store
 const middleware = applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase }));
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(reduxFirestore(firebase), middleware));
+const store = createStore(rootReducer, persistedState, composeEnhancers(reduxFirestore(firebase), middleware));
+
+// Save state to local storage
+const saveToLocalStorage = state => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem("state", serializedState);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 // props for ReactReduxFirebaseProvider
 export const rrfConfig = {
