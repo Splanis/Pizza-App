1599e314df81d6042a496a5e4cfe247543c0a551
Marios G <splanis5@gmail.com>
2020-03-09 19:44:55 +0200

    CartItems component created

---
+++ src/pages/dashboard/cart/CartItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { useDispatch } from "react-redux";
+
+import { incrementQuantityAction, decrementQuantityAction, removeFromCartAction } from "../../../store/actions/cartActions";
+
+import { List, Icon, Button } from "semantic-ui-react";
+
+const CartItem = ({ id, name, price, quantity }) => {
+    const dispatch = useDispatch();
+
+    return (
+        <List.Item style={{ margin: 10 }} key={id}>
+            <List.Content>
+                <List.Header as="a">{name}</List.Header>
+                <List.Description style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <Icon name="euro" /> {price}
+                    <Button.Group size="mini" style={{ margin: "0 10px" }}>
+                        <Button onClick={() => dispatch(decrementQuantityAction(id))}>
+                            <Icon name="minus" style={{ marginRight: 10 }} />
+                        </Button>
+                        <Button.Or text={quantity} />
+                        <Button onClick={() => dispatch(incrementQuantityAction(id))}>
+                            <Icon name="add" style={{ marginLeft: 10 }} />
+                        </Button>
+                        <Button onClick={() => dispatch(removeFromCartAction(id))} icon="remove" />
+                    </Button.Group>
+                </List.Description>
+            </List.Content>
+        </List.Item>
+    );
+};
+
+export default CartItem;
--- src/pages/dashboard/cart/index.js
+++ src/pages/dashboard/cart/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+import CartItem from "./CartItem";
 
 import { Segment, List, Icon, Button, Header } from "semantic-ui-react";
-
-import { incrementQuantityAction, decrementQuantityAction } from "../../../store/actions/cartActions";
 
 const Cart = () => {
     const cartItems = useSelector(state => state.cart.cartItems);
     const totalCost = useSelector(state => state.cart.totalCost);
-    const dispatch = useDispatch();
 
     return (
         <Segment placeholder style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -21,23 +20,7 @@ const Cart = () => {
                     <div>
                         <Header style={{ textAlign: "center" }}>Καλάθι</Header>
                         {cartItems.map(item => (
-                            <List.Item style={{ margin: 10 }} key={item.id}>
-                                <List.Content>
-                                    <List.Header as="a">{item.name}</List.Header>
-                                    <List.Description style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                        <Icon name="euro" /> {item.price}
-                                        <Button.Group size="mini" style={{ margin: "0 10px" }}>
-                                            <Button onClick={() => dispatch(decrementQuantityAction(item.id))}>
-                                                <Icon name="minus" style={{ marginRight: 10 }} />
-                                            </Button>
-                                            <Button.Or text={item.quantity} />
-                                            <Button onClick={() => dispatch(incrementQuantityAction(item.id))}>
-                                                <Icon name="add" style={{ marginLeft: 10 }} />
-                                            </Button>
-                                        </Button.Group>
-                                    </List.Description>
-                                </List.Content>
-                            </List.Item>
+                            <CartItem key={item.id} id={item.id} name={item.name} quantity={item.quantity} price={item.price} />
                         ))}
                         <Segment style={{ margin: 10, display: "flex", justifyContent: "center" }}>
                             Συνολικό πόσο: {totalCost.toFixed(2)} <Icon name="euro" />
